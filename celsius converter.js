d2fb3f3cef9b2c6356c2ac261f81fb056abf73ed
SJ NORTH <samanthajmnorth@gmail.com>
2016-01-06 22:28:48 +0200

    celsius

---
+++ celsius converter.js
@@ -0,0 +1,13 @@
+function convert(celsius) {
+  
+var fahrenheit = celsius * 9 / 5 + 32;
+ 
+  if ( typeof celsius !== 9 / 5 + 32 ) {
+  return fahrenheit;
+  } else {
+    return celsius;
+  }
+}
+
+// Change the inputs below to test code
+convert(0);
